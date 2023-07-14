dc10a168bc657052545c6977aafdcc0085b6bc4a
Rabsterd <ksm6798@gamil.com>
2023-07-14 14:31:14 +0900

    first commit

---
+++ src/adu/kh/basic/JavaTest.java
@@ -0,0 +1,59 @@
+package adu.kh.basic;
+
+// 한줄 주석 코드 설명 작성
+/*
+ * 범위 주석
+ * */
+
+// Java 코드 실행 순서 : 
+// 코드 작성 -> ctrl + f11 실행
+
+public class JavaTest {
+	
+// class : 자바 코드가 작성되는 영역
+
+public static void main(String[] args) {
+	// 메인 메서드 : 자바 애플리케이션을 실행하기
+	//위해 반드시 필요한 메서드
+	
+	//ctrl + alt + 방향키 : 작성한 방향으로 해당 줄 복사
+	System.out.println("ddsgdgdR");
+	System.out.println("ddsgdgdR");
+	
+	System.out.println("-----------------");
+	
+	//숫자 연산
+	System.out.println("1 + 2");
+	System.out.println(1 + 2);
+	
+	// "" 안에 작성된 코드는 단순 문자열 인식
+	// "" 안에 작성되지 않은 코드는 숫자, 변수로 인식
+	
+	System.out.println(50-20);
+	System.out.println(132*24);
+	System.out.println(120/40);
+	System.out.println(120%40);
+
+	//""(문자열) + 숫자 함께 작성
+	
+	System.out.println("14 * 19 = " + 266);
+	System.out.println("14 * 19 =  " + 14 * 19);
+	
+	System.out.println("90 + 70 + 65 = " + (90 + 70 + 65));
+	
+	// 자바는 사칙연산의 우선순위를 그대로 따른다
+	//-> 우선 계산을 원하면 () 사용
+	
+	// + 기호의 역할
+	// -> 숫자 + 수자 = 덧셈 연산 결과
+	// -> 문자열 + 숫자 or 
+	// 문자열 + 문자열 --> 이어쓰기
+	
+	
+	
+	
+}
+
+
+
+}
